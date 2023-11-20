9b32705f640f80ab58174fb180c680270d1bde83
alcinacarlos <calcrom0607@g.educaand.es>
2023-11-20 17:09:44 +0100

    ez

--- src/ej_3_1_12.py
+++ src/ej_3_1_12.py
@@ -1,9 +1,9 @@
-vector1 = (1, 2, 3)
-vector2 = (-1, 0, 2)
+vector1 = ((1, 2), (3, 4), (5, 6))
+vector2 = ((-1, 0), (0, 1) ,(1, 1))
 
-def producto_escalar(vector1:tuple, vector2:tuple) -> str:
-    escalar = [vector1[i] * vector2[i] for i in range(len(vector1))]
-    return f"El producto escalar de {vector1} y {vector2} es: {escalar}"
+def producto_escalar(vector1: tuple, vector2: tuple) -> tuple:
+    resultado = tuple(tuple(vector1[i][x] * vector2[i][x] for x in range(len(vector1[i]))) for i in range(len(vector1)))
+    return resultado
 
 def main():
     print(producto_escalar(vector1, vector2))
